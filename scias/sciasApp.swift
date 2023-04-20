dfa87d58e5bb865601b87cdef8af26698a4606da
LinRuiiXin <1303058293@qq.com>
2023-04-20 11:15:40 +0800

    Initial Commit

---
+++ scias/sciasApp.swift
@@ -0,0 +1,17 @@
+//
+//  sciasApp.swift
+//  scias
+//
+//  Created by 林瑞鑫 on 2023/4/20.
+//
+
+import SwiftUI
+
+@main
+struct sciasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
